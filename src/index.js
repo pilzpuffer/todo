5de86a00d6d2379432263cb0befbe9e3b7089a38
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2026-06-13 01:47:21 +0300

    Set up a function to handle modal click

--- src/index.js
+++ src/index.js
@@ -4,6 +4,40 @@ import { projectSetUp } from "./projectCreate.js";
 
 window.addEventListener("load", function() {
 
-    projectSetUp();
+    let handleModalClick = function() {
+
+        let newProjectButton = document.querySelector("#newProject");
+        let projectModal = document.querySelector("#projectOpen"); 
+        let projectModalClose = document.querySelector("#projectClose");
+        let projectModalSubmit = document.querySelector("#projectSubmit");
+
+        let modals = {
+                project: {
+                    button: newProjectButton,
+                    type:  projectModal,
+                    close:  projectModalClose,
+                    submit: projectModalSubmit,
+                },
+                // task: {
+                //     button: newTaskButton,
+                //     type:  taskModal,
+                //     close:  taskModalClose,
+                //     submit: taskModalSubmit,
+                // }
+        }
+
+            Object.keys(modals).forEach(modal => {
+                let currentModal = modals[modal];
+                currentModal.button.addEventListener("click", () => {
+                    currentModal.type.showModal();
+                })
+
+                currentModal.close.addEventListener("click", () => {
+                    currentModal.type.close();
+                })
+        });
+    }
+    
+    handleModalClick();
 
 })
