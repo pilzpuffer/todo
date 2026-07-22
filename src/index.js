4cc77f45a63a90e4fecd4d30a16cf5a85eb51d77
pilzpuffer <82716742+pilzpuffer@users.noreply.github.com>
2026-07-22 04:31:31 +0300

    Set up 'main' project folder on page load

--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ import { newProject } from "./projectCreate.js";
 import { createTaskForm } from "./taskFormSetup.js";
 import { createProjectForm } from "./projectFormSetup.js";
 
+
+
 window.addEventListener("load", function() {
     let noteData = document.querySelector("#taskInfo");
     let projectData = document.querySelector("#projectInfo");
 
+    let createProjectManually = function(name) {
+    let projectSubmitButton = document.querySelector("#projectSubmit");
+
+    document.forms['projectInfo'].elements['projectTitle'].value = name;
+
+    projectSubmitButton.click();
+}
+
     createTaskForm();
-    createProjectForm(); //adjust styling for this
+    createProjectForm();
+    createProjectManually('main');
 
 })
